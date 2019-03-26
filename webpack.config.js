58491954420197d0334048fef1c184a930d52726
Miguel Rodrigues <miguel.rodrigues@sky.uk>
2019-03-26 18:24:51 +0000

    styles now injected and loaded through webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,14 @@ module.exports = {
     entry: "./src/index.js",
     output: {
         filename: "main.js",
-        path: path.resolve(__dirname,"dist")
-    }
+        path: path.resolve(__dirname,"dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,  //this is dinamicaly loading the CSS and injecting it in the final app build inside the header
+                use: ['style-loader', 'css-loader'], //they are executed from the end to the beginning of the array
+            },
+        ],
+    },
 }
